957db7dbdb27b9a7108dff4f02bc9120b27fefaf
André ÃLacerda <dedelacerda95@gmail.com>
2023-11-16 05:10:30 -0300

    implementei seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,19 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 if Rails.env.development?
-  Subject.destroy_all
-  Competency.destroy_all
 
-  6.times do |i|
-    Subject.find_or_create_by!(subject_name: "Subject #{i + 1}")
-    puts "Created Subject #{i + 1}"
-  end
+  Subject.find_or_create_by!(subject_name: "Física")
+  Subject.find_or_create_by!(subject_name: "Matemática")
+  Subject.find_or_create_by!(subject_name: "Português")
+  Subject.find_or_create_by!(subject_name: "Química")
+  Subject.find_or_create_by!(subject_name: "Biologia")
+  Subject.find_or_create_by!(subject_name: "História")
 
-  6.times do |i|
-    Competency.find_or_create_by!(competency_name: "Compentency #{i + 1}")
-    puts "Created Competency #{i + 1}"
-  end
+  Competency.find_or_create_by!(competency_name: "Raciocínio lógico")
+  Competency.find_or_create_by!(competency_name: "Interpretação de texto")
+  Competency.find_or_create_by!(competency_name: "Conhecimento de fórmulas")
+  Competency.find_or_create_by!(competency_name: "Conhecimento de teorias")
+  Competency.find_or_create_by!(competency_name: "Conhecimento de regras gramaticais")
+  Competency.find_or_create_by!(competency_name: "Habilidades em conta matemática"
+  )
 end
